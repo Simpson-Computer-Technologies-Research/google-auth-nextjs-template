04c2ab47dd3ae8b147dbe6c08f2b7db3d50241cf
Tristan <heytristaann@gmail.com>
2023-12-22 15:14:17 -0400

    c

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,16 +1,18 @@
-import { Permission } from "@/lib/types";
 import "next-auth";
 
 declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
+  interface User {
+    id: string | null;
+    secret: string | null;
+    name: string | null;
+    email: string | null;
+    image: string | null;
+  }
+
   interface Session {
-    user: {
-      secret: string | null;
-      name: string | null;
-      email: string | null;
-      image: string | null;
-    };
+    user: User;
   }
 }
